1e4998a5496252a1ecae07ac5a74561f6d0cc3f4
Dimitrije Markovic <126205228+Dimitrije108@users.noreply.github.com>
2023-11-01 19:10:02 +0100

    Add slider button functionality
    Slider button will now create a new grid of square divs based on input

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const container = document.querySelector('.container');
 const slider = document.querySelector('#slider');
 const sliderValue = document.querySelector('#slider-value');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('input');
+let inputValue = 16;
 
 slider.addEventListener('input', function(){
     sliderValue.textContent = slider.value;
 });
 
-function grid() {
-    for (let i = 0; i < slider.value; i++) {
+btn.addEventListener('click', function(){
+    inputValue = input.value;
+    grid(inputValue);
+})
+
+function grid(inputValue) {
+    if (container.hasChildNodes()) {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+    }
+    for (let i = 0; i < inputValue; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
-        for (let j = 0; j < slider.value; j++) {
+        for (let j = 0; j < inputValue; j++) {
             const square = document.createElement('div');
             square.classList.add('square');
             row.appendChild(square);
@@ -19,4 +32,4 @@ function grid() {
     }
 }
 
-console.log(grid());
+grid(inputValue);
